97c845a07f58b913dffd301b789341de2803060a
janneke-hub <86882640+janneke-hub@users.noreply.github.com>
2021-09-24 12:12:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const makePizza = function() {
+    console.log('make the dough');
+    console.log('make the sauce');
+    console.log('rolling Out the Dough');
+    console.log('make the Topping');
+    console.log('bake the Pizza');
+};
+
+makePizza();
+makePizza();
+
+const makeSushi = function() {
+    console.log('Prep the Rice & Nori');
+    console.log('Spread the Rice');
+    console.log('Add Fillings to the Bottom Quarter of the Nori');
+    console.log('Roll the Sushi Part Way');
+    console.log('Roll the Sushi the Rest of the Way');
+    console.log('Move to Cutting Board');
+    console.log('Cut the Sushi');
+    console.log('Serve');
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+
+
